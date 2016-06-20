1aacfb425359f24173d25c8011353a2ec995f24f
mybabyhate <couleuria@gmail.com>
2016-06-20 07:59:50 +0700

    a

---
+++ DoAnWeb/DoAnWeb/Models/Helper/Poco_Index_Page.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace DoAnWeb.Models.Helper
+{
+    public class Poco_Index_Page
+    {
+        public List<tbl_SanPhams> DanhSachSanPhamMoiNhat { get; set; }
+        public List<tbl_SanPhams> DanhSachSanPhamXemNhieuNhat { get; set; }
+        public List<tbl_SanPhams> DanhSachSanPhamBanChayNhat { get; set; }
+    }
+}
