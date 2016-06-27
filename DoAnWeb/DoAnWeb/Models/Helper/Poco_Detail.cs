17ea0c3574481c9a1368c0191d17f71a2829ec73
mybabyhate <couleuria@gmail.com>
2016-06-28 05:51:54 +0700

    TimKiem

---
+++ DoAnWeb/DoAnWeb/Models/Helper/Poco_Detail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoAnWeb.Models.Helper
+{
+    public class Poco_Detail
+    {
+        public tbl_SanPhams ThongTinSanPham { get; set; }
+        public string TenNhaSanXuat { get; set; }
+        public string TenLoaiSanPham { get; set; }
+    }
+}
